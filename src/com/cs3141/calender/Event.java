cced5a6be6d873da7133647e4e4856d0a3918f62
Tim Bradt <tjbradt@mtu.edu>
2013-09-19 11:42:45 -0400

    Started Event Class and Calender class

---
+++ src/com/cs3141/calender/Event.java
@@ -0,0 +1,19 @@
+package com.cs3141.calender;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tim
+ * Date: 9/18/13
+ * Time: 2:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Event extends Main {
+	Date m_date;
+	String m_name;
+	String m_place;
+	String m_discription;
+	boolean m_repeat;
+
+}
