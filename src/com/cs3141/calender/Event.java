49f6b7cb94a2a53c6f179d81e4049de7bd41bbf1
Tim Bradt <tjbradt@mtu.edu>
2013-09-23 14:55:47 -0400

    Event Class  started

--- src/com/cs3141/calender/Event.java
+++ src/com/cs3141/calender/Event.java
@@ -7,13 +7,27 @@ import java.util.Date;
  * User: Tim
  * Date: 9/18/13
  * Time: 2:09 PM
- * To change this template use File | Settings | File Templates.
  */
-public class Event extends Main {
-	Date m_date;
-	String m_name;
-	String m_place;
-	String m_discription;
-	boolean m_repeat;
+public class Event{
+	protected int id;
+	public Date m_date;
+	public String m_name;
+	public String m_place;
+	public String m_discription;
+
+	Event(Date _date, String _name, String _place, String _dis)
+	{
+		id = _date.hashCode() + _name.hashCode() + _place.hashCode() + _dis.hashCode();
+		m_date = _date;
+		m_name = _name;
+		m_place = _place;
+		m_discription = _dis;
+	}
+
+	int getId()
+	{
+		return id;
+	}
+	//boolean m_repeat;
 
 }
